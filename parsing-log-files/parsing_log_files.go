154157fcb6dedcbe10ec04de4571a6f29618a802
Tor Einar Huus <tor@huus.me>
2024-11-01 17:34:40 +0100

    started work on plf

--- parsing-log-files/parsing_log_files.go
+++ parsing-log-files/parsing_log_files.go
@@ -1,11 +1,30 @@
 package parsinglogfiles
 
+import (
+	"regexp"
+)
+
 func IsValidLine(text string) bool {
-	panic("Please implement the IsValidLine function")
+	validPattern := `^\[(TRC|DBG|INF|WRN|ERR|FTL)\]`
+
+	re := regexp.MustCompile(validPattern)
+	return re.MatchString(text)
 }
 
 func SplitLogLine(text string) []string {
-	panic("Please implement the SplitLogLine function")
+	validPattern := `^<[-=*~]*>$`
+
+	re := regexp.MustCompile(validPattern)
+	sections := re.Split(text, -1)
+
+	var result []string
+	for _, section := range sections {
+		if section != "" {
+			result = append(result, section)
+		}
+	}
+
+	return result
 }
 
 func CountQuotedPasswords(lines []string) int {
