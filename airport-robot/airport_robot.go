21ebf79db0ddc3b62553c9afd6f47a2ea25f7cf9
Tor Einar Huus <tor@huus.me>
2024-11-03 14:46:14 +0100

    airport-robot implementedW

---
+++ airport-robot/airport_robot.go
@@ -0,0 +1,34 @@
+package airportrobot
+
+import "fmt"
+
+type Greeter interface {
+	LanguageName() string
+	Greet(visitorName string) string
+}
+
+func SayHello(visitorName string, greeter Greeter) string {
+	return fmt.Sprintf("I can speak %s: %s", greeter.LanguageName(), greeter.Greet(visitorName))
+}
+
+type Italian struct {
+}
+
+func (i Italian) LanguageName() string {
+	return "Italian"
+}
+
+func (i Italian) Greet(visitorName string) string {
+	return fmt.Sprintf("Ciao %s!", visitorName)
+}
+
+type Portuguese struct {
+}
+
+func (p Portuguese) LanguageName() string {
+	return "Portuguese"
+}
+
+func (p Portuguese) Greet(visitorName string) string {
+	return fmt.Sprintf("Olá %s!", visitorName)
+}
